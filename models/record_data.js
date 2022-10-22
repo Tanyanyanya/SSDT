60c07050ad6eb3ec7648ea92c3466eb091cd01d9
Tanyanyanya <76079713+Tanyanyanya@users.noreply.github.com>
2022-10-23 00:52:25 +0200

    create model for records

---
+++ models/record_data.js
@@ -0,0 +1,8 @@
+const record = [
+    { id: 1, user_id: 1, category_id:1 , date: '2020-02-02', sum: 320 },
+    { id: 2, user_id: 1, category_id:2 , date: '2020-02-12', sum: 400 },
+    { id: 3, user_id: 2, category_id:1 , date: '2021-11-05', sum: 487 },
+    { id: 4, user_id: 2, category_id:2 , date: '2022-01-01', sum: 4300 }
+]
+
+module.exports = record
